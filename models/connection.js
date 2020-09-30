b159dc6ece8ef0f0f56239df0a70dce0e8abdc92
vinicius-vasconcelos <viniciussouzav@gmail.com>
2020-09-30 13:31:23 -0300

    Instalando e configurando .env - iniciando projeto

---
+++ models/connection.js
@@ -0,0 +1,11 @@
+require('dotenv/config');
+
+const config = {
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  host: process.env.HOSTNAME,
+  port: 33060,
+  socketPath: '/var/run/mysqld/mysqld.sock',
+};
+
+module.exports = { config };
