22e0f57b61065fc6aa280ed9544acd2ed13b21ce
vinicius-vasconcelos <viniciussouzav@gmail.com>
2020-10-05 02:34:40 -0300

    fix cc js

--- models/RecipeModel.js
+++ models/RecipeModel.js
@@ -22,8 +22,8 @@ const findById = async (id) => {
       .execute();
 
     const row = await stmt.fetchOne();
-    const [_id, user_id, user, name, ingredients, instructions] = row;
-    return ({ id: _id, userId: user_id, user, name, ingredients, instructions });
+    const [_id, userId, user, name, ingredients, instructions] = row;
+    return ({ id: _id, userId, user, name, ingredients, instructions });
   } catch (error) {
     return false;
   }
