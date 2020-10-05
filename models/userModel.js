cfd9bcdad4019ca78bca7e857f00d90854b165d4
vinicius-vasconcelos <viniciussouzav@gmail.com>
2020-10-04 21:48:46 -0300

    Finalizando desafio 1

--- models/userModel.js
+++ models/userModel.js
@@ -1,21 +1,25 @@
-/* Quando você implementar a conexão com o banco, não deve mais precisar desse objeto */
-const TEMP_USER = {
-  id: 'd2a667c4-432d-4dd5-8ab1-b51e88ddb5fe',
-  email: 'taylor.doe@company.com',
-  password: 'password',
-  name: 'Taylor',
-  lastName: 'Doe',
-};
-
-/* Substitua o código das funções abaixo para que ela,
-de fato, realize a busca no banco de dados */
+const conn = require('./connection');
 
 /**
  * Busca um usuário através do seu email e, se encontrado, retorna-o.
  * @param {string} email Email do usuário a ser encontrado
  */
 const findByEmail = async (email) => {
-  return TEMP_USER;
+  try {
+    const db = await conn();
+    const stmt = await db
+      .getTable('users')
+      .select(['id', 'email', 'password', 'first_name', 'last_name'])
+      .where('email = :email')
+      .bind('email', email)
+      .execute();
+    const row = await stmt.fetchOne();
+    const [id, _email, password, name, lastName] = row;
+
+    return ({ id, email: _email, password, name, lastName });
+  } catch (error) {
+    return false;
+  }
 };
 
 /**
@@ -23,7 +27,22 @@ const findByEmail = async (email) => {
  * @param {string} id ID do usuário
  */
 const findById = async (id) => {
-  return TEMP_USER;
+  try {
+    const db = await conn();
+    const stmt = await db
+      .getTable('users')
+      .select(['id', 'email', 'password', 'first_name', 'last_name'])
+      .where('id = :id')
+      .bind('id', id)
+      .execute();
+
+    const row = await stmt.fetchOne();
+    const [_id, login, password, name, lastName] = row;
+
+    return ({ id: _id, email: login, password, name, lastName });
+  } catch (error) {
+    return false;
+  }
 };
 
 module.exports = {
